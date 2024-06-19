67b96e5e307501062f4e3d40e7e8a14f2c124d3d
Gaurav Soni <gauravsoni7763@gmail.com>
2024-06-19 11:48:44 +0545

    Installed Hotjar with npm

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Hotjar from '@hotjar/browser';
+
+const siteId = 5029209;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
