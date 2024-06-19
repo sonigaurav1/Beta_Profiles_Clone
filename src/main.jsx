b13242bae48c08f7ac5e39ce037676a5abefdbeb
Gaurav Soni <gauravsoni7763@gmail.com>
2024-06-19 12:15:09 +0545

    seperate Hotjar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Hotjar from '@hotjar/browser';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Hotjar from "./helpers/Hotjar.jsx";
 
-const siteId = 5029209;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Hotjar />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
